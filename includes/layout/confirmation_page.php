ac296190b5b79acb2bfe451967a72d7f79fd5a2e
David Gaskin <davidburkegaskin@gmail.com>
2016-06-16 21:48:20 -0700

    Style confirmation page; Improve UX/UI; Display text after image load

--- includes/layout/confirmation_page.php
+++ includes/layout/confirmation_page.php
@@ -1,17 +1,16 @@
 <?php
 	// if no message, EXIT script
 		if( !isset($success_msg) && !isset($error_msg) ){
-
 			$location = 'http://songfarm.ca';
-			header("Location: " . $location);
-			exit;
+			// header("Location: " . $location);
+			// exit;
 
-			// temp
-			// $success_msg = $error_msg = '';
+			/* for testing */
+			$success_msg = $error_msg = '';
 
 		}
 ?>
-<!doctype>
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -20,104 +19,74 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 		<link rel="stylesheet" href="../../public/css/global.css" type="text/css">
+		<link rel="stylesheet" href="../../public/css/confirmation_page.css" type="text/css">
 		<script type="text/javascript" src="../../public/js/jquery-1.11.3.min.js"></script>
 		<script type="text/javascript" src="../../public/js/spin.js/spin.min.js"></script>
-		<style>
-			html, body{
-				max-width: initial;
-				max-height: 100%;
-				overflow: hidden;
-			}
-			article#logoContainer{
-				height: 300px;
-				width: 100%;
-				margin: 3.5em 0 1em;
-			}
-			article#logoContainer > div{
-				width: 456px;	height: 100%;
-				margin: 0 auto;	overflow: hidden;
-			}
-			article#userMessage{
-				font-family: 'museo_sans300';	text-align: center;
-				font-size: 2em;	color: #3F9FEB;
-			}
-			article#userMessage p{
-				font-family: inherit;	line-height: 1.5em;
-				font-weight: 600;	margin: 0 auto;
-				display: block;
-				max-width: 65%;
-				padding: 0 1em;
-			}
-			article#userMessage a{	color: #7CC919; transition: color .250s; }
-			article#userMessage a:hover{ color: #666666; transition: color .250s; }
-
-			div#spinLoader{
-				position: relative;
-				top: -50px; bottom: 0;
-				display: block;
-				width: 100%; height: 300px;
-				display: none;
-			}
-			div#spinLoader p{
-				position: absolute; bottom: 1.5em;
-				left:50%;	transform: translateX(-50%);
-				font-size: 1.75em; font-style: italic;
-			}
-			span#ellipsis{ position: absolute; font-style: inherit; }
-
-			@media screen and (max-width: 1120px){
-				article#userMessage p{
-					max-width: initial;
-				}
-			}
-		</style>
 	</head>
 	<body>
-		<article id="logoContainer">
-			<div>
-				<img src="../../public/images/songfarm_logo_l.png" alt="Songfarm Logo" height="auto" width="100%">
+		<main>
+			<p id="loadMessage">Loading.. Please wait.</p>
+			<section id="logoContainer">
+				<div>
+					<img src="../../public/images/songfarm_logo_l.png" alt="Songfarm Logo" height="auto" width="100%">
+				</div>
+			</section>
+			<section id="userMessage">
+				<?php
+					/**
+					* NOTE: Write code so that if there is an error message, the message stays on the screen for longer
+					*/
+					if ( $error_msg && is_array($error_msg) ) {
+						foreach ($error_msg as $error) {
+							echo '<p data-flag="error">'.$error.'</p>';
+						}
+					} elseif ( $success_msg || is_array($success_msg) ) {
+						if( is_array($success_msg) )
+						foreach ($success_msg as $success_msg) {
+							echo '<p>'.$success_msg.'</p>';
+						} else {
+							echo '<p>'.$success_msg.'</p>';
+						}
+					}
+					else
+					{
+						// redirect somewhere
+						// redirectTo('../public/index.php');
+						echo '<p data-flag="error">No Message - success or error - was detected. Doing a redirect now.</p>';
+					}
+				?>
+			</section>
+			<div id="spinLoader">
+				<p>redirecting<span id="ellipsis"></span></p>
 			</div>
-		</article>
-		<article id="userMessage">
-			<?php
-				/**
-				* NOTE: Write code so that if there is an error message, the message stays on the screen for longer
-				*/
-				if ( $error_msg && is_array($error_msg) ) {
-					foreach ($error_msg as $error) {
-						echo '<p data-flag="error">'.$error.'</p>';
-					}
-				} elseif ( $success_msg || is_array($success_msg) ) {
-					if( is_array($success_msg) )
-					foreach ($success_msg as $success_msg) {
-						echo '<p>'.$success_msg.'</p>';
-					} else {
-						echo '<p>'.$success_msg.'</p>';
-					}
-				}
-				else
-				{
-					// redirect somewhere
-					// redirectTo('../public/index.php');
-					echo '<p data-flag="error">No Message - success or error - was detected. Doing a redirect now.</p>';
-				}
-			?>
-			<!-- jQuery plug-in for spin loader -->
-		</article>
-		<div id="spinLoader">
-			<p>redirecting<span id="ellipsis"></span></p>
-		</div>
+		</main>
 	</body>
 	<script>
-		// on page load event, set timer.
 		window.onload = function(){
+			var loadMessage = document.getElementById("loadMessage");
+			var img = document.getElementsByTagName("img");
+			// hide the user message div
+			var userMsg = document.getElementById("userMessage");
+			userMsg.style.display = "none";
+			// // create a load message
+			// var p = document.createElement("p");
+			// var text = document.createTextNode("Loading.. Please wait");
+			// p.appendChild(text);
+			// insert load message after <main>
+			// var loadMessage = document.getElementsByTagName("main")[0].insertBefore(p, document.getElementsByTagName("main")[0].childNodes[1]);
+			// wait until image has loaded to show user message div
+			if( img[0].complete ){
+				// hide the load message
+				loadMessage.style.display = "none";
+				// show user message
+				userMsg.style.display = "block";
+			}
 
+			/* script for spin loader */
 			var target = document.getElementById('spinLoader');
-
 			var div = $('div#spinLoader');
 			var span = $('span#ellipsis');
 			var timerDuration;
-
 			/* set timer duration dependant on type of message */
 			var dataFlag = $('#userMessage p').data('flag');
 			if( dataFlag == 'error'){
@@ -127,13 +96,10 @@
 			} else {
 				timerDuration = 5000;
 			}
-			// console.log(timerDuration);
-
 			/*** redirect location ***/
 				// live config:
 					redirectURL = 'http://test.songfarm.ca';
 					// redirectURL = 'http://songfarm.ca';
-
 				// test config:
 					// redirectURL = '../public';
 
@@ -148,25 +114,21 @@
 
 				/* interval for ellipsis creation */
 				setInterval(function(){
-
 					if(counter <= 2){
 						// append a period on to the element
 						$(span).append('.');
 						counter++;
-					}
-					else
-					{
+					}	else {
 						var replace = $(span).text().replace('...','');
 						$(span).text(replace);
 						counter = 0;
 					}
-
 				}, 1000);
 
 				/* timer for redirect */
-				setTimeout(function(){
-					window.location.replace(redirectURL);
-				},3500);
+				// setTimeout(function(){
+				// 	window.location.replace(redirectURL);
+				// },3500);
 
 			},timerDuration);
 
